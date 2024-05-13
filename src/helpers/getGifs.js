d8948ac5a6f108b5794248af3ced30717064909c
hirochi04 <santiagohirochi04@gmail.com>
2024-05-13 13:56:44 -0600

    going forward

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,24 @@
+// Definimos una función asíncrona getGifs que toma una categoría como argumento
+export const getGifs = async( category ) => {
+    // Construimos la URL para la API de Giphy con la categoría y un límite de 10
+   imágenes
+    const url =
+   `https://api.giphy.com/v1/gifs/search?api_key=HhyTxP09g6k5BOulTMImZ9Pl1neE72Q7&q=${ category
+   }&limit=10`;
+    // Hacemos una petición a la API de Giphy
+    const resp = await fetch( url );
+    // Extraemos los datos de la respuesta en formato JSON
+    const { data } = await resp.json();
+    // Mapeamos los datos a un nuevo arreglo de objetos con id, título y URL de cada
+   imagen
+    const gifs = data.map( img => ({
+    id: img.id,
+    title: img.title,
+    url: img.images.downsized_medium.url
+    }));
+   
+    // Devolvemos el arreglo de objetos
+    return gifs;
+   }
+
+   
