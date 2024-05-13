5c1bc541fa3b1652f5a3e42e31e4b0670825ffff
hirochi04 <santiagohirochi04@gmail.com>
2024-05-13 13:32:24 -0600

    starting project

---
+++ src/ProyectoGif.jsx
@@ -0,0 +1,16 @@
+
+
+export const ProyectoGif = () => {
+    return (
+    <>
+    {/* Título */}
+    <h1>ProyectoGif</h1>
+    {/* Input */}
+    {/* Listado de Gifs */}
+    {/* Git Item */}
+    </>
+   
+    )
+   }
+
+   export default ProyectoGif
